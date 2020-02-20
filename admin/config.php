a452f80baa619f62181ad971fb13ff16320482bd
Vitaliy <vitaliy.lambov@mail.ru>
2020-02-20 18:59:11 +0200

    version v1

---
+++ admin/config.php
@@ -0,0 +1,37 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore/admin/');
+define('HTTP_CATALOG', 'http://ocstore/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore/admin/');
+define('HTTPS_CATALOG', 'http://ocstore/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/Server/OSPanel/domains/ocStore/admin/');
+define('DIR_SYSTEM', 'D:/Server/OSPanel/domains/ocStore/system/');
+define('DIR_IMAGE', 'D:/Server/OSPanel/domains/ocStore/image/');
+define('DIR_STORAGE', 'D:/Server/OSPanel/domains/storage/');
+define('DIR_CATALOG', 'D:/Server/OSPanel/domains/ocStore/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ocstoredb');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
